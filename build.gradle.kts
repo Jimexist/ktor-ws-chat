8d237fa377d15f477eb7a8bd7d688159b11ee898
Jiayu Liu <jiayu@hey.com>
2022-01-01 20:55:43 +0800

    add exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,17 @@ repositories {
 }
 
 application {
-
   mainClass.set("io.ktor.server.netty.EngineMain")
 }
+
+val exposedVersion: String by project
 
 dependencies {
   implementation(kotlin("stdlib"))
   implementation("io.ktor:ktor-server-core:1.6.7")
   implementation("io.ktor:ktor-server-netty:1.6.7")
   implementation("ch.qos.logback:logback-classic:1.2.10")
+  implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+  implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+  implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 }
