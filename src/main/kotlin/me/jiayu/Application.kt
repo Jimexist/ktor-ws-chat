b30633324dc92262b5dd9cf1e3a8cddbcc87828f
Jiayu Liu <jiayu@hey.com>
2022-01-01 20:54:27 +0800

    main setup

---
+++ src/main/kotlin/me/jiayu/Application.kt
@@ -0,0 +1,17 @@
+package me.jiayu;
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
+}
